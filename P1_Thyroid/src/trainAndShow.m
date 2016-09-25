19f9111393ec826e409c92c2c6084b7da5da2b00
JavierMartinezRiberas <jmr0082@alu.ubu.es>
2016-09-25 19:04:10 +0200

    P1: Changes to trainAndShow to reduce variance in results.

--- P1_Thyroid/src/trainAndShow.m
+++ P1_Thyroid/src/trainAndShow.m
@@ -1,9 +1,24 @@
+n=10;
 [ inputs, targets ] = thyroid_dataset;
+avg_c = 0;
+avg_cm=[0 0 0; 0 0 0; 0 0 0];
 for train_f={'trainscg', 'traincgp'}
     train_func = train_f{1};
     for n_neurons=10:10:30
-        net = autoTrain(n_neurons, inputs, targets, train_func);
-        plotconfusion(targets , net(inputs))
-        waitforbuttonpress
+        for reduce_variance=1:1:n
+            net = autoTrain(n_neurons, inputs, targets, train_func);
+            [C,CM,IND,PER] = confusion(targets, net(inputs));
+            avg_c  = avg_c  + C ;
+            avg_cm = avg_cm + CM;
+            %plotconfusion(targets , net(inputs))
+        end
+        avg_c = avg_c/n;
+        avg_cm = avg_cm./n;
+        disp(strcat('Para ''',int2str(n_neurons),''' neuronas entrenadas con ''', ...
+        train_func, ''' el error medio es: ''', num2str(C),''''));
+        disp(strcat('La matriz de confusion media es: '))
+        avg_cm
+        %waitforbuttonpress
     end
+    
 end
