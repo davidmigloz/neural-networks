548743f4710af1db65567c22fdbfdfd8a5c2ddf3
davidmigloz <me@davidmiguel.com>
2016-09-25 16:57:49 +0200

    P1: add procedure description to report

--- P1_Thyroid/src/autoTrain.m
+++ P1_Thyroid/src/autoTrain.m
@@ -1,6 +1,6 @@
 function [ net ] = autoTrain(n_neurons, inputs, targets, train_function,...
     ratios)
-%AUTOTRAIN Funcion de entrenamiento automatizado
+%AUTOTRAIN Funcion de entrenamiento automatizado.
 %   Recibe como parametos: 
 %   	  n_neurons        ->  Numero de neuronas
 %   	  inputs           ->  Entradas del dataset
@@ -11,23 +11,23 @@ function [ net ] = autoTrain(n_neurons, inputs, targets, train_function,...
 %                               el valor de la suma de ellos debera ser 
 %                               menor de 1
 %   
-%   Devuelve la red ya entrenada
+%   Devuelve la red ya entrenada.
 %
-%   Cabe considerar que la funcion abrira una ventana de como esta yendo el
+%   Cabe considerar que la funcion abrira una ventana de como está yendo el
 %   aprendizaje.
-%   Si no se introduce train_function se usa 'trainscg'
-%   Si no se introducen los ratios se usan [0.70 , 0.15]
+%   Si no se introduce train_function se usa 'trainscg' por defecto.
+%   Si no se introducen los ratios se usan [0.70 , 0.15].
 %   
 
-    if ~exist('train_function', 'var') 
-        net = patternnet(n_neurons);
+    if exist('train_function', 'var') 
+        net = patternnet(n_neurons, train_function);
     else
-        net = patternnet(n_neurons,train_function);
+        net = patternnet(n_neurons);
     end
     if exist('ratios', 'var')
         net.divideParam.trainRatio = ratios(1);
         net.divideParam.valRatio = ratios(2);
         net.divideParam.testRatio = 1-ratios(1)-ratios(2);
     end
-    net = train(net,inputs ,targets);
+    net = train(net, inputs ,targets);
 end
--- P1_Thyroid/src/trainAndShow.m
+++ P1_Thyroid/src/trainAndShow.m
@@ -1,7 +1,7 @@
 [ inputs, targets ] = thyroid_dataset;
 for train_f={'trainscg', 'traincgp'}
     train_func = train_f{1};
-    for n_neurons=1:9:27
+    for n_neurons=10:10:30
         net = autoTrain(n_neurons, inputs, targets, train_func);
         plotconfusion(targets , net(inputs))
         waitforbuttonpress
